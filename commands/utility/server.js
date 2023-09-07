5494ae07aec7b039aa7b64579bbd9386f4967e10
Michael Connolly <mpc63@uakron.edu>
2023-09-06 23:56:47 -0400

    Refactor server command to use embeds

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -6,8 +6,32 @@ module.exports = {
     .setDescription('Provides information about the server.')
     .setDMPermission(false),
   async execute(interaction) {
-    await interaction.reply(
-      `Server: ${interaction.guild.name}\nVerified: ${interaction.guild.verified}\nMembers: ${interaction.guild.memberCount}`
-    );
+    const owner = await interaction.guild.fetchOwner();
+
+    const embed = new EmbedBuilder()
+      .setColor(0x6ab7dd)
+      .setTitle(`${interaction.guild.name}`)
+      .setThumbnail(interaction.guild.iconURL())
+      .addFields(
+        {
+          name: 'Owner',
+          value: `${owner.user.username}`,
+          inline: true,
+        },
+        {
+          name: 'Verified',
+          value: `${interaction.guild.verified}`,
+          inline: true,
+        },
+        { name: 'Members', value: `${interaction.guild.memberCount}` },
+        {
+          name: 'Created',
+          value: `<t:${Math.floor(
+            interaction.guild.createdTimestamp / 1000
+          )}:R>`,
+        }
+      );
+
+    await interaction.reply({ embeds: [embed] });
   },
 };

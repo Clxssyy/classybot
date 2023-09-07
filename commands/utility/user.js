61581ca417ce95ccaad579c47193714db06fa882
Michael Connolly <mpc63@uakron.edu>
2023-09-06 20:18:39 -0400

    Refactor User command

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -1,27 +1,25 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
+  data: new SlashCommandBuilder()
+    .setName('user')
+    .setDescription('Provides information about the user.')
+    .addUserOption((option) =>
+      option
         .setName('user')
-        .setDescription('Provides information about the user.')
-        .addUserOption((option) =>
-            option
-                .setName('user')
-                .setDescription(
-                    'The member you wish to view (default: user who ran command)'
-                )
+        .setDescription(
+          'The member you wish to view (default: user who ran command)'
         )
-        .setDMPermission(false),
-    async execute(interaction) {
-        const member = interaction.options.getMember('user') ?? '';
-        if (member != '') {
-            await interaction.reply(
-                `Username: ${member.user.username}\nJoined: ${member.joinedAt}`
-            );
-        } else {
-            await interaction.reply(
-                `Username: ${interaction.user.username}\nJoined: ${interaction.member.joinedAt}`
-            );
-        }
-    },
+    )
+    .setDMPermission(false),
+  async execute(interaction) {
+    const member = interaction.options.getMember('user') ?? interaction.member;
+    const flags = member.user.flags.toArray();
+
+    await interaction.reply(
+      `Username: ${member.user.username}\nJoined: <t:${Math.floor(
+        member.joinedTimestamp / 1000
+      )}:R> Flags: ${flags}`
+    );
+  },
 };
